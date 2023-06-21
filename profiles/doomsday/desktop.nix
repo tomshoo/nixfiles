73da55673a9f646bf5c7786c1c6143510865d4bb
Shubhanshu Tomar <tomarshubhanshu@protonmail.ch>
2023-06-21 17:08:50 +0530

    profile[doomsday]: set displayManager to use `gnome` as default

--- profiles/doomsday/desktop.nix
+++ profiles/doomsday/desktop.nix
@@ -1,37 +1,45 @@
-{ pkgs, ... } :
-let extensions = with pkgs.gnomeExtensions;
-  [ appindicator
-    quick-settings-tweaker
-    blur-my-shell
-    caffeine
-    clipboard-indicator
-    dotspaces
-    just-another-search-bar
-    maximize-to-empty-workspace
-    rounded-window-corners
-  ];
-in {
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-  };
+{ pkgs,
+  ...
+} : let
+  extensions = with pkgs.gnomeExtensions;
+    [ appindicator
+      quick-settings-tweaker
+      blur-my-shell
+      caffeine
+      clipboard-indicator
+      dotspaces
+      just-another-search-bar
+      maximize-to-empty-workspace
+      rounded-window-corners
+      disable-menu-switching
+      move-clock
+    ];
+in
+{ services.xserver =
+    { enable = true;
+      layout = "us";
+      displayManager.gdm.enable = true;
+      displayManager.defaultSession = "gnome";
+      desktopManager.gnome.enable = true;
+    };
 
   services.udev.packages = with pkgs.gnome;
-    [ gnome-settings-daemon ];
+    [ gnome-settings-daemon
+    ];
 
-  environment.gnome.excludePackages = with pkgs; [
-    gnome-console
-    gnome.gnome-music
-  ];
+  environment.gnome.excludePackages = with pkgs;
+    [ gnome-console
+      gnome.gnome-music
+    ];
 
-  environment.systemPackages = with pkgs;
-    [ blackbox-terminal
-      amberol
-      endeavour
-      gnome.gnome-tweaks
-    ] ++ extensions;
+  environment.systemPackages =
+    extensions
+    ++(with pkgs;
+      [ blackbox-terminal
+        amberol
+        endeavour
+        gnome.gnome-tweaks
+      ]);
 
   xdg.portal.enable = true;
   programs.gnome-terminal.enable = true;
