c1983fc94577e19bb562306a6c42757578e337ee
Shubhanshu Tomar <tomarshubhanshu@protonmail.ch>
2023-07-05 15:56:48 +0530

    add new package for `tsman`

--- personal/applications/tmux/default.nix
+++ personal/applications/tmux/default.nix
@@ -1,16 +1,24 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  tmux-session-manager = pkgs.writeShellScriptBin
+    "tsman"
+    (builtins.readFile ./tmux-session-manager.sh);
+in {
   programs.tmux = {
-    enable = true;
-    baseIndex = 1;
-    keyMode = "vi";
-    prefix = "C-Space";
+    enable        = true;
+    baseIndex     = 1;
+    keyMode       = "vi";
+    prefix        = "C-Space";
     sensibleOnTop = true;
-    terminal = "screen-256color";
+    terminal      = "screen-256color";
+
     plugins = with pkgs.tmuxPlugins;
-      [{ plugin = vim-tmux-navigator;
-          extraConfig = builtins.readFile ./tmux.vim-tmux-navigator.conf;
+      [{ plugin      = vim-tmux-navigator;
+         extraConfig = builtins.readFile ./tmux.vim-tmux-navigator.conf;
       }];
 
     extraConfig = builtins.readFile ./tmux.default.conf;
+
   };
+
+  home.packages = [ tmux-session-manager ];
 }
