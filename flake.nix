bfec0b56d6a5087d05f753beec15880be7d814f2
Shubhanshu Tomar <tomarshubhanshu@protonmail.ch>
2023-07-05 15:56:13 +0530

    reformat

--- flake.nix
+++ flake.nix
@@ -2,34 +2,35 @@
   description = "Configuration flake for the system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    nur.url = "github:nix-community/NUR";
-    wolfangaukang.url = "git+https://codeberg.org/wolfangaukang/nix-agordoj?ref=main";
+    nixpkgs.url          = "github:nixos/nixpkgs/nixos-23.05";
+    nur.url              = "github:nix-community/NUR";
+    wolfangaukang.url    = "git+https://codeberg.org/wolfangaukang/nix-agordoj?ref=main";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
+    home-manager         = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... } @ inputs:
-    let system = "x86_64-linux";
-        pkgconfig = {
-          config.allowUnfree = true;
-          localSystem = {
-            inherit system;
-          };
-        };
+  outputs = { self,
+              nixpkgs,
+              nixpkgs-unstable,
+              ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgconfig =
+      { config.allowUnfree = true;
+        localSystem.system = system;
+      };
 
-        pkgs = import nixpkgs (pkgconfig);
-        pkgs-unstable = import nixpkgs-unstable (pkgconfig);
+    pkgs          = import nixpkgs (pkgconfig);
+    pkgs-unstable = import nixpkgs-unstable (pkgconfig);
 
-        inherit (nixpkgs) lib;
-  in {
-    nixosConfigurations = import ./profiles {
-      inherit lib pkgs pkgs-unstable system;
-      inherit (inputs) home-manager nur wolfangaukang;
-    };
+    inherit (nixpkgs) lib;
+  in 
+  { nixosConfigurations = import ./profiles
+      { inherit lib pkgs pkgs-unstable system;
+        inherit (inputs) home-manager nur wolfangaukang;
+      };
   };
 }
